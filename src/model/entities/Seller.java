841e33d3dbeaceedb75a9ddc63d97f1d409ab8f7
Felipe Jesus <felipe1smg1g@gmail.com>
2020-05-16 14:13:19 -0400

    findByDepartment implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -6,17 +6,16 @@ import java.util.Date;
 public class Seller implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
-
+	
 	private Department department;
-
+	
 	public Seller() {
-
+		
 	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
