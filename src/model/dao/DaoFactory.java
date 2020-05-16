4108d00b83a58ed761215c2c784b90de897642d8
Felipe Jesus <felipe1smg1g@gmail.com>
2020-05-16 13:47:34 -0400

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,42 +2,13 @@ package model.dao;
 
 import java.util.List;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 import model.entities.Seller;
 
 public class DaoFactory {
-public static SellerDao createSellerDao() {
-	return new SellerDaoJDBC() {
-		
-		@Override
-		public void update(Seller obj) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void insert(Seller obj) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public Seller findById(Integer id) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public List<Seller> findAll() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public void deleteById(Integer id) {
-			// TODO Auto-generated method stub
-			
-		}
-	};
-}
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+
+	}
 }
